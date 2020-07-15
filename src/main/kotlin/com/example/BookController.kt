5aee300be2d0965ec4ef8c19a1197a311e4c689c
Aldrin Misquitta <aldrinm@gmail.com>
2020-07-15 18:44:24 +0400

    Separate book controller

---
+++ src/main/kotlin/com/example/BookController.kt
@@ -0,0 +1,26 @@
+package com.example
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import javax.inject.Inject
+
+@Controller("/book")
+class BookController {
+
+    @Inject
+    lateinit var bookRepository: BookRepository
+
+    @Post
+    fun createBook(book: Book): Book {
+        val savedBook = bookRepository.save(book)
+
+        return savedBook
+    }
+
+    @Get
+    fun listBooks(): List<Book> {
+        return bookRepository.findAll().toList()
+    }
+}
+
